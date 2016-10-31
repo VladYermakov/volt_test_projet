e82f6d009df0c609762e8042b08b5f66ed3de534
Vlad Yermakov <yermakov.v.o@gmail.com>
2016-11-01 01:26:02 +0300

    configure mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   address:        'smtp.gmail.com',
-  port:           '465',
+  port:           '587',
   domain:         ENV['domain'],
   authentication: :plain,
   user_name:      ENV['username'],
