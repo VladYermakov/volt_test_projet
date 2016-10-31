23817116393b399ab2562f9515cfc0dbb53983b7
Vlad Yermakov <yermakov.v.o@gmail.com>
2016-11-01 01:53:16 +0300

    configure mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :address              => 'smtp.mailgun.org',
+  :enable_starttls_auto => true,
+  :address              => 'smtp.gmail.com',
   :port                 => 587,
-  :domain               => 'app400f24dcf9d84429b8dccf28cc767a93.mailgun.org',
+  :domain               => 'mail.google.com',
   :authentication       => :plain,
-  :user_name            => 'postmaster@app400f24dcf9d84429b8dccf28cc767a93.mailgun.org',
-  :password             => 'ef2bd8c057c76ed645d7103c9ed4b5a3',
-  :enable_starttls_auto => true
+  :user_name            => ENV['username'],
+  :password             => ENV['password']
 }
