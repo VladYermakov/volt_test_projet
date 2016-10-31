6b16b2382d812000e61a9c90d48c113fba6aa93e
Vlad Yermakov <yermakov.v.o@gmail.com>
2016-11-01 01:47:58 +0300

    configure mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :user_name      => 'a9bb3ca5950f8b',
-  :password       => 'b93ddddcf60351',
-  :address        => 'mailtrap.io',
-  :domain         => 'mailtrap.io',
-  :port           => '2525',
-  :authentication => :cram_md5
+  :address              => 'smtp.mailgun.org',
+  :port                 => 587,
+  :domain               => 'app400f24dcf9d84429b8dccf28cc767a93.mailgun.org',
+  :authentication       => :plain,
+  :user_name            => 'postmaster@app400f24dcf9d84429b8dccf28cc767a93.mailgun.org',
+  :password             => 'ef2bd8c057c76ed645d7103c9ed4b5a3',
+  :enable_starttls_auto => true
 }
