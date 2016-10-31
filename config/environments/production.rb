f6450bbe5136e8abde52c560e879214358f4fb6f
Vlad Yermakov <yermakov.v.o@gmail.com>
2016-11-01 00:58:09 +0300

    configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = "volt_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "volt_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'https://gentle-harbor-11044.herokuapp.com/' }
