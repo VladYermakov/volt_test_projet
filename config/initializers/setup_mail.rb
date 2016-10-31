9acff3d49ebec5b4a1caecd67db481a88cde3b29
Vlad Yermakov <yermakov.v.o@gmail.com>
2016-11-01 01:35:06 +0300

    configure mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
   :user_name      => 'a9bb3ca5950f8b',
   :password       => 'b93ddddcf60351',
   :address        => 'mailtrap.io',
