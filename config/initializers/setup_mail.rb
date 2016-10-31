dc1e1031b29a5fa02742cc7175fcda4ef5e6ac78
Vlad Yermakov <yermakov.v.o@gmail.com>
2016-11-01 01:32:39 +0300

    configure mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  address:        'smtp.gmail.com',
-  port:           '587',
-  domain:         ENV['domain'],
-  authentication: :plain,
-  user_name:      ENV['username'],
-  password:       ENV['password'],
-  ssl:            true,
-  enable_starttls_auto: true
+  :address              => 'smtp.gmail.com',
+  :port                 => 587,
+  :domain               => ENV['domain'],
+  :authentication       => :plain,
+  :user_name            => ENV['username'],
+  :password             => ENV['password'],
+  :enable_starttls_auto => true
 }
