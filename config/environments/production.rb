5137edd0a7268fe38125042e5e701f7a2db92546
Vlad Yermakov <yermakov.v.o@gmail.com>
2016-11-01 00:55:21 +0300

    configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,9 +53,15 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "volt_#{Rails.env}"
+  config.active_job.queue_adapter     = :resque
+  config.active_job.queue_name_prefix = "volt_#{Rails.env}"
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'https://gentle-harbor-11044.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
